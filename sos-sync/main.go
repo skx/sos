8007d066339ce37a74a80010e8b5461dfb239555
Steve Kemp <steve@steve.org.uk>
2016-06-05 09:09:43 +0300

    Sketched out mirroring implementation.
    TODO - Actually mirror.
    
    Right now the fetching of contents is hardwired, via a JSON file,
    and the mirroring only handles determining where to copy files to
    rather than doing so.
    
    This updates #19.

--- sos-sync/main.go
+++ sos-sync/main.go
@@ -6,11 +6,130 @@
 package main
 
 import (
+	"../blobservers"
+	"encoding/json"
 	"flag"
 	"fmt"
-	"github.com/skx/sos/blobservers"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"strings"
 )
+
+var verbose *bool
+
+//
+// Does the given server contain the given object?
+//
+func HasObject(server string, object string) bool {
+
+	response, err := http.Head(server + "/blob/" + object)
+	if err != nil {
+		fmt.Printf("Error Fetching: %s/blob/%s\n", server, object)
+		return false
+	} else {
+		if response.StatusCode == 200 {
+			fmt.Printf("\tObject %s is present on %s\n", object, server)
+			return true
+		} else {
+			fmt.Printf("\tObject %s is missing on %s\n", object, server)
+			return false
+		}
+	}
+}
+
+//
+// Mirror the given object.
+//
+func MirrorObject(src string, dst string, obj string) {
+	// NOP
+	if *verbose {
+		fmt.Printf("\t\tMirroring %s from %s to %s\n",
+			obj, src, dst)
+	}
+}
+
+//
+// Sync the members of the set we're given.
+//
+func SyncGroup(servers []blobservers.BlobServer) {
+	//
+	// If we're being verbose show the members
+	//
+	if *verbose {
+		for _, s := range servers {
+			fmt.Printf("\tGroup member: %s\n", s.Location)
+		}
+	}
+
+	//
+	// For each server - download the content-list here
+	//
+	//   key is server-name
+	//   val is array of strings
+	//
+	objects := make(map[string][]string)
+
+	for _, s := range servers {
+		type list_strings []string
+		var tmp list_strings
+
+		file, err := ioutil.ReadFile("test.json")
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = json.Unmarshal(file, &tmp)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		//
+		// OK we have a string-array - we'll add that
+		// to our list.
+		//
+		for _, i := range tmp {
+
+			if *verbose {
+				fmt.Printf("\tServer %s has %s\n", s.Location, i)
+			}
+			objects[s.Location] = append(objects[s.Location], i)
+		}
+	}
+
+	//
+	// Right we have a list of servers.
+	//
+	// For each server we also have the list of objects
+	// that they contain.
+	//
+	for _, server := range servers {
+
+		// The objects on this server
+		var obs = objects[server.Location]
+
+		// For each object.
+		for _, i := range obs {
+
+			//
+			//  Mirror the object to every server that is not itself
+			//
+			for _, mirror := range servers {
+
+				//
+				// Ensure that src != dst.
+				//
+				if mirror.Location != server.Location {
+
+					// If the object is missing.
+					if !HasObject(mirror.Location, i) {
+						MirrorObject(server.Location, mirror.Location, i)
+					}
+				}
+
+			}
+		}
+	}
+}
 
 /**
  * Entry point to our code.
@@ -21,7 +140,7 @@ func main() {
 	// Parse our command-line arguments.
 	//
 	blob := flag.String("blob-server", "", "Comma-separated list of blob-servers to contact.")
-	verbose := flag.Bool("verbose", false, "Be verbose?")
+	verbose = flag.Bool("verbose", false, "Be verbose?")
 	flag.Parse()
 
 	//
@@ -46,7 +165,7 @@ func main() {
 	//
 	// Show the blob-servers.
 	//
-	if ( *verbose ) {
+	if *verbose {
 		fmt.Printf("\t% 10s - %s\n", "group", "server")
 		for _, entry := range blobservers.Servers() {
 			fmt.Printf("\t% 10s - %s\n", entry.Group, entry.Location)
@@ -54,4 +173,19 @@ func main() {
 	}
 
 	fmt.Println("TODO: Write a replication tool!")
+
+	//
+	// Get a list of groups.
+	//
+	for _, entry := range blobservers.Groups() {
+
+		if *verbose {
+			fmt.Printf("Syncing group: %s\n", entry)
+		}
+
+		//
+		// For each group, get the members, and sync them.
+		//
+		SyncGroup(blobservers.GroupMembers(entry))
+	}
 }
