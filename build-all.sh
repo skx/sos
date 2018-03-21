396d13d7ece92a8ba8e2eb40f0e4224a879148c0
Steve Kemp <steve@steve.org.uk>
2018-03-21 16:24:21 +0200

    Build/Release binaries for more platforms.

---
+++ build-all.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+BUILD_PLATFORMS="linux windows darwin freebsd"
+BUILD_ARCHS="amd64 386"
+
+# For each platform
+for OS in ${BUILD_PLATFORMS[@]}; do
+
+    # For each arch
+    for ARCH in ${BUILD_ARCHS[@]}; do
+
+        #
+        # Windows binaries would be more useful if they had
+        # an .EXE suffix.
+        #
+        SUFFIX=""
+        if [ "$OS" = "windows" ]; then
+            SUFFIX=".exe"
+        fi
+
+        # Get the dependencies
+        GOARCH=${ARCH} GOOS=${OS} CGO_ENABLED=0 go get -t -v -d $(go list ./...)
+
+        # Do the build
+        GOARCH=${ARCH} GOOS=${OS} CGO_ENABLED=0 go build -ldflags "-X main.version=$(git describe --tags)" -o sos-${OS}-${ARCH}${SUFFIX} .
+    done
+done
