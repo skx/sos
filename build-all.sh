f990eddf02d198291d523f2d8f4410e8bf46b278
Steve Kemp <steve@steve.org.uk>
2018-03-22 08:26:32 +0200

    Minor improvement to building binaries

--- build-all.sh
+++ build-all.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# The basename of our binary
+BASE="sos"
 
+# Get the dependencies
+go get -t -v -d $(go list ./...)
+
+
+#
+# We build on multiple platforms/archs
+#
 BUILD_PLATFORMS="linux windows darwin freebsd"
 BUILD_ARCHS="amd64 386"
 
@@ -10,18 +19,29 @@ for OS in ${BUILD_PLATFORMS[@]}; do
     # For each arch
     for ARCH in ${BUILD_ARCHS[@]}; do
 
-        #
-        # Windows binaries would be more useful if they had
-        # an .EXE suffix.
-        #
-        SUFFIX=""
-        if [ "$OS" = "windows" ]; then
-            SUFFIX=".exe"
+        # Setup a suffix for the binary
+        SUFFIX="${OS}"
+
+        # i386 is better than 386
+        if [ "$ARCH" = "386" ]; then
+            SUFFIX="${SUFFIX}-i386"
+        else
+            SUFFIX="${SUFFIX}-${ARCH}"
         fi
 
-        echo "Building for $OS $ARCH"
+        # Windows binaries should end in .EXE
+        if [ "$OS" = "windows" ]; then
+            SUFFIX="${SUFFIX}.exe"
+        fi
 
-        # Do the build
-        GOARCH=${ARCH} GOOS=${OS} CGO_ENABLED=0 go build -ldflags "-X main.version=$(git describe --tags)" -o sos-${OS}-${ARCH}${SUFFIX} -tags=${OS} .
+        echo "Building for ${OS} [${ARCH}] -> ${BASE}-${SUFFIX}"
+
+        # Run the build
+        export GOARCH=${ARCH}
+        export GOOS=${OS}
+        export CGO_ENABLED=0
+
+        go build -ldflags "-X main.version=$(git describe --tags)" -o "${BASE}-${SUFFIX}" -tags=${OS} .
+
     done
 done
